4b6d522d95a904b452b62bae63c0559ab859ab26
inseo <wisberry2022@gmail.com>
2022-10-09 14:59:22 +0900

    10/09 upload

--- src/detail/practice/Board/OtherLink/post/Post.js
+++ src/detail/practice/Board/OtherLink/post/Post.js
@@ -1,12 +1,10 @@
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
 
-const Post = () => {
-  const postingData = useSelector(state => state.review);
+const Post = ({ postingData }) => {
   const params = useParams();
   return (
     <div>
-      {console.log(`POST 내부 데이터:`, postingData[params.id - 1])}
+      {console.log(postingData[params.id - 1])}
       hello
     </div>
   )
--- src/detail/practice/Practice.js
+++ src/detail/practice/Practice.js
@@ -3,19 +3,70 @@ import Board from "./Board/Board";
 import RegisterReview from "./Board/OtherLink/registerReview/RegisterReview";
 import Post from "./Board/OtherLink/post/Post";
 import { Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { UPDATE } from "../../store/module/reviewSlice";
 
 const Practice = () => {
+  const postingData = useSelector(state => state.review);
+  const dispatch = useDispatch();
   const location = useLocation();
+
+  useEffect(() => {
+    axios.get('/mm_practice')
+      .then((res) => (dispatch(UPDATE(res.data))));
+
+    // const exam = [
+    //   {
+    //     "_id": {
+    //       "$oid": "6340a5d5e6a290dfcba72877"
+    //     },
+    //     "title": "감사합니다!",
+    //     "author": "왕인서",
+    //     "email": "ground444@naver.com",
+    //     "password": "103030200",
+    //     "contents": "수행과 실천 2일차~\n넘 좋아요~ 여러분들도 하세요!",
+    //     "date": "2022-10-08",
+    //     "__v": 0
+    //   },
+    //   {
+    //     "_id": {
+    //       "$oid": "6340a5d5e6a290dfcba72877"
+    //     },
+    //     "title": "수고하셨습니다!",
+    //     "author": "김주은",
+    //     "email": "ground444@naver.com",
+    //     "password": "103030200",
+    //     "contents": "수행과 실천 2일차~\n넘 좋아요~ 여러분들도 하세요!",
+    //     "date": "2022-10-08",
+    //     "__v": 0
+    //   },
+    //   {
+    //     "_id": {
+    //       "$oid": "6340a5d5e6a290dfcba72877"
+    //     },
+    //     "title": "수행5일차",
+    //     "author": "오창석",
+    //     "email": "ground444@naver.com",
+    //     "password": "103030200",
+    //     "contents": "수행과 실천 2일차~\n넘 좋아요~ 여러분들도 하세요!",
+    //     "date": "2022-10-08",
+    //     "__v": 0
+    //   },
+    // ];
+  }, [location.pathname, dispatch])
+
   return (
     <div className="practice_wrapper">
       {location.pathname === '/mm_practice' ?
         <>
           <Intro />
-          <Board />
+          <Board postingData={postingData} />
         </> :
         <Routes>
           <Route path="reviewRegister" element={<RegisterReview />} />
-          <Route path="post/:id" element={<Post />} />
+          <Route path="post/:id" element={<Post postingData={postingData} />} />
         </Routes>
       }
     </div>
